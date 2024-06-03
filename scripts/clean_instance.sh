8dcfc6b35ef6013577ea3d0f4f2b055dd6ca59aa
BinuChristin <Binu.christin@faithinfotech.in>
2024-06-03 16:49:34 +0530

    added2

--- scripts/clean_instance.sh
+++ scripts/clean_instance.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-#
-sudo rm -rf /home/ubuntu/carsandbikesblog/*
+sudo systemctl daemon-reload
+sudo rm -f /etc/nginx/sites-enabled/default
+
+sudo cp /home/ubuntu/carsandbikesblog/nginx/nginx.conf /etc/nginx/sites-available/carsandbikesblog
+sudo ln -s /etc/nginx/sites-available/carsandbikesblog /etc/nginx/sites-enabled/
+
+# sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+# sudo nginx -t
+sudo gpasswd -a www-data ubuntu
+sudo systemctl restart ngin
--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -2,12 +2,5 @@
 #!/usr/bin/bash
 
 sudo systemctl daemon-reload
-sudo rm -f /etc/nginx/sites-enabled/default
-
-sudo cp /home/ubuntu/carsandbikesblog/nginx/nginx.conf /etc/nginx/sites-available/carsandbikesblog
-sudo ln -s /etc/nginx/sites-available/carsandbikesblog /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-#sudo nginx -t
-sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
 
